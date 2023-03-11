37d5f3c0c8a98974a11d05c23fd110b80cbdf303
Arturo Rodriguez <arturorodr96@gmail.com>
2023-03-11 14:34:52 -0600

    prod acma url

--- src/api/AcmaClient.js
+++ src/api/AcmaClient.js
@@ -11,7 +11,7 @@ export class AcmaClient {
     this.cookies = new Cookies();
     const token = this.cookies.get('jwt')
     this.axios = axios.create({
-      baseURL: process.env.ACMA_BASE_URL || 'http://locahost/api/v1',
+      baseURL: 'https://acma.grupopromer.com',
       headers: { 'Authorization': `Bearer ${token}`}
     });
   }
