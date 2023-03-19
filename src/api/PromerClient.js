eb1d751e591f09db25bc15b2e809e0c9e7e91a2c
Josue Ramirez Salvador <josuers823@gmail.com>
2023-03-19 00:23:46 -0600

    Connect Import and get all lands

--- src/api/PromerClient.js
+++ src/api/PromerClient.js
@@ -1,11 +1,37 @@
 import axios from 'axios';
+import Cookies from 'universal-cookie';
 
 export class PromerClient {
   axios = {};
-  
+
   constructor() {
+    this.cookies = new Cookies();
+    const token = this.cookies.get('jwt')
     this.axios = axios.create({
-      baseURL: process.env.PROMER_BASE_URL || 'http://locahost:3000/api/v1',
+      baseURL: 'http://localhost:3000/v1',
+      headers: { 'Authorization': `Bearer ${token}`}
     });
   }
+
+  async importFile(fileImport) {
+    try {
+      const formData = new FormData();
+      formData.append("fileImport", fileImport);
+      console.log(this.axios.baseURL)
+      const response = await this.axios.post('/lands/import-create-lands', formData, {
+        headers: {
+          "Content-Type": "multipart/form-data",
+        }});
+      return response?.data;
+    } catch (error) {
+      console.error(error);
+      throw error;
+    }
+  }
+
+  async findAllLands() {
+    const response = await this.axios.get('/lands');
+    return response;
+  }
+  
 }
